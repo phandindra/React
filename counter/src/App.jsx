ef273d160f753b1d5bcbe1f575c299bc9fdf0531
Indra <indraphand24@gmail.com>
2024-08-25 22:03:12 +0530

    counter react added

---
+++ counter/src/App.jsx
@@ -0,0 +1,37 @@
+
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+ const [counter, setCounter]= useState(5)
+ 
+  
+ const  addvalue = ()=>{
+  if(counter<20){
+    setCounter(counter+1);
+    console.log("clicked")
+  }
+  }
+
+  const removevalue=()=>{
+   if(counter>0){
+    setCounter(counter-1);
+    console.log("clicked")
+   }
+  }
+
+  return (
+    <>
+        <h1>Counter</h1>
+
+        <h2>current value :{counter}</h2>
+
+        <button onClick={addvalue}>ADD</button>
+        
+        <button onClick={removevalue}>Remove</button>
+
+    </>
+  )
+}
+
+export default App
